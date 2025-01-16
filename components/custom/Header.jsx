fc28200e8d69fa554f9b352d17f343ab27678d73
swastideep maharana <swastideep67maharana@gmail.com>
2025-01-17 00:31:31 +0530

    #added hover

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -5,11 +5,24 @@ import Colors from "@/data/Colors";
 
 const Header = () => {
   return (
-    <div className="p-4 flex justify-between items-center">
-      <Image src={"/logo.png"} alt="Logo" width={40} height={40} />
+    <div className="p-4 flex justify-between items-centershadow-md rounded-lg transition-all duration-500">
+      <Image
+        src={"/logo.png"}
+        alt="Logo"
+        width={40}
+        height={40}
+        className="transition-transform duration-300 hover:scale-110"
+      />
       <div className="flex gap-5">
-        <Button variant="ghost">Sign In</Button>
-        <Button className="bg-blue-400">Get Started</Button>
+        <Button
+          variant="ghost"
+          className="text-white-500 hover:bg-blue-100 hover:text-blue-700 transition-all duration-300 transform hover:scale-105"
+        >
+          Sign In
+        </Button>
+        <Button className="bg-blue-500 text-white hover:bg-blue-600 transition-all duration-300 transform hover:scale-105">
+          Get Started
+        </Button>
       </div>
     </div>
   );
